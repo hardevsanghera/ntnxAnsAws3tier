4d9b934565ab90e060727f4ac5f737e5ff7f378d
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2022-08-24 23:10:17 -0700

    Update setupMysql.sh

--- setupMysql.sh
+++ setupMysql.sh
@@ -3,9 +3,8 @@
 #Original scripts from Nutanix CALM early version used, modified for ansible deployment.
 #Target is an aws ec2 instance
 #$1 is the mysql database password, of the homestead database
-#hardev@nutanix.com Auh'22
-
-#set -ex
+#hardev@nutanix.com Aug'22
+set -ex
 
 sudo yum install -y "http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm"
 sudo yum update -y
@@ -13,16 +12,29 @@ sudo setenforce 0 || true
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 sudo systemctl stop firewalld || true
 sudo systemctl disable firewalld || true
-sudo yum install -y mysql-community-server.x86_64
+sudo yum install -y mysql-community-server.x86_64 unzip zip lvm2 lsof ntp
+sudo ntpdate -u -s 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org
+sudo systemctl enable ntpd
+sudo systemctl restart ntpd
+#Setup sshd to allow password logins / root
+sudo sed -i 's/\#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+sudo bash -c 'echo "Defaults umask_override" >> /etc/sudoers'
+sudo bash -c 'echo "Defaults umask=0002" >> /etc/sudoers'
+
+#Alter mysql to support Nutanix DB Service
+sudo bash -c 'echo "[mysqld]" >> /etc/my.cnf'
+sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/my.cnf'
+sudo bash -c 'echo "log-bin=mysql-bin.log" >> /etc/my.cnf'
 sudo /bin/systemctl start mysqld
 sudo /bin/systemctl enable mysqld
 
-#Mysql secure installation
-mysql -u root<<-EOF
-UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root';
-FLUSH PRIVILEGES;
-EOF
-mysql -u root -p$1 <<-EOF
+#Mysql create datbase for the app and also access for db root / homestead users
+mysql -u root <<-EOF
+UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='homestead';
+CREATE USER 'root'@'%' IDENTIFIED BY '$1'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$1';
 CREATE DATABASE homestead;
 GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'%' identified by '$1';
 GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'localhost' identified by '$1';
