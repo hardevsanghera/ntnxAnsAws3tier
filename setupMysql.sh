a0000a5b05a500e3f6ccc88a818a80175c79f7c0
hardevsanghera <hardev_sanghera@yahoo.co.uk>
2022-08-26 10:19:33 -0700

    Update setupMysql.sh

--- setupMysql.sh
+++ setupMysql.sh
@@ -4,8 +4,10 @@
 #Target is an aws ec2 instance
 #$1 is the mysql database password, of the homestead database
 #hardev@nutanix.com Aug'22
+
 set -ex
 
+#Insall packages, disable selinux and firewall then setup NTP
 sudo yum install -y "http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm"
 sudo yum update -y
 sudo setenforce 0 || true
@@ -16,33 +18,28 @@ sudo yum install -y mysql-community-server.x86_64 unzip zip lvm2 lsof ntp
 sudo ntpdate -u -s 0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org
 sudo systemctl enable ntpd
 sudo systemctl restart ntpd
-#Setup sshd to allow password logins / root
-#sudo sed -i 's/\#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
-#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-sudo bash -c 'echo "Defaults umask_override" >> /etc/sudoers'
-sudo bash -c 'echo "Defaults umask=0002" >> /etc/sudoers'
 
-#Alter mysql to support Nutanix DB Service
+#Set mySQL switches for renote access and future support for DB Service, start mySQL
 sudo bash -c 'echo "[mysqld]" >> /etc/my.cnf'
 sudo bash -c 'echo "bind-address = 0.0.0.0" >> /etc/my.cnf'
 sudo bash -c 'echo "log-bin=mysql-bin.log" >> /etc/my.cnf'
 sudo /bin/systemctl start mysqld
 sudo /bin/systemctl enable mysqld
 
-#Mysql create datbase for the app and also access for db root / homestead users
-mysql -u root <<-EOF
+#Setup mySQL database and users, password for root and user homestead is the same and passed in via $1
+mysql -u root<<-EOF
 UPDATE mysql.user SET Password=PASSWORD('$1') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1', '%');
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+CREATE USER 'root'@'%' IDENTIFIED BY '$1'; 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$1';
 FLUSH PRIVILEGES;
-EOF
-mysql -u root -p$1 <<-EOF
 CREATE DATABASE homestead;
-GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'localhost' identified by '$1';
-GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'127.0.0.1' identified by '$1';
-GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'%' identified by '$1';
+GRANT ALL PRIVILEGES ON homestead.* TO 'homestead'@'%' IDENTIFIED BY '$1';
 FLUSH PRIVILEGES;
 EOF
+
+#Throw in a restart of mySQL!
+sudo /bin/systemctl restart mysqld
